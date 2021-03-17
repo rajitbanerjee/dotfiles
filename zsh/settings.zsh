76843b0be608f1bd07003b49e081bf74d16cdf5c
Rajit Banerjee <rajit.banerjee@ucdconnect.ie>
2021-03-17 15:52:40 +0000

    Skip verification of insecure directories zsh

--- zsh/settings.zsh
+++ zsh/settings.zsh
@@ -1,3 +1,6 @@
+# Skip verification of insecure directories (macOS error)
+ZSH_DISABLE_COMPFIX="true"
+
 # If you come from bash you might have to change your $PATH.
 export PATH="$HOME/bin:/usr/local/bin:$PATH"
 
