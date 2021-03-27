c74c5ba9cc858099fc2300ee4e5e17e5aa6c8011
Rajit Banerjee <rajit.banerjee@ucdconnect.ie>
2021-03-27 12:14:54 +0000

    Conda init is common for all shells

--- shell/external.sh
+++ shell/external.sh
@@ -46,4 +46,18 @@ elif [[ "$(uname -s)" == "Linux" ]]; then
     export NVM_DIR=~/.nvm
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+    ## Conda setup
+    __conda_setup="$(~/.local/share/conda/bin/conda shell.$0 hook 2> /dev/null)"
+    if [ $? -eq 0 ]; then
+        eval "$__conda_setup"
+    else
+        if [ -f ~/.local/share/conda/etc/profile.d/conda.sh ]; then
+            . ~/.local/share/conda/etc/profile.d/conda.sh
+        else
+            export PATH=~/.local/share/conda/bin:$PATH
+        fi
+    fi
+    unset __conda_setup
+
 fi
--- zsh/settings.zsh
+++ zsh/settings.zsh
@@ -1,36 +1,19 @@
-# Skip verification of insecure directories (macOS error)
-ZSH_DISABLE_COMPFIX="true"
 
-# If you come from bash you might have to change your $PATH.
 export PATH=~/bin:/usr/local/bin:$PATH
-
-# Path to your oh-my-zsh installation.
 export ZSH=~/.oh-my-zsh
 
-# Set name of the theme to load --- if set to "random", it will
-# load a random theme each time oh-my-zsh is loaded, in which case,
-# to know which specific one was loaded, run: echo $RANDOM_THEME
-# See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
+ZSH_DISABLE_COMPFIX="true" # Skip verification of insecure directories (macOS error)
 ZSH_THEME="powerlevel10k/powerlevel10k"
+ENABLE_CORRECTION="true"
+ZSH_CUSTOM=~/.zsh/custom
 
 [[ ! -f ~/.zsh/optional.zsh ]] || source ~/.zsh/optional.zsh
 
-# Uncomment the following line to enable command auto-correction.
-ENABLE_CORRECTION="true"
-
-# Apt package suggestions
 if [[ "$(uname -s)" == "Linux" ]]; then
+    # Apt package suggestions
     source /etc/zsh_command_not_found
 fi
 
-# Would you like to use another custom folder than $ZSH/custom?
-ZSH_CUSTOM=~/.zsh/custom
-
-# Which plugins would you like to load?
-# Standard plugins can be found in $ZSH/plugins/
-# Custom plugins may be added to $ZSH_CUSTOM/plugins/
-# Example format: plugins=(rails git textmate ruby lighthouse)
-# Add wisely, as too many plugins slow down shell startup.
 plugins=(
     docker                      # Docker completion
     docker-compose
@@ -43,19 +26,3 @@ plugins=(
 
 source $ZSH/oh-my-zsh.sh
 
-if [[ "$(uname -s)" == "Linux" ]]; then
-    # >>> conda initialize >>>
-    # !! Contents within this block are managed by 'conda init' !!
-    __conda_setup="$('/home/banerjee/.local/share/conda/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
-    if [ $? -eq 0 ]; then
-        eval "$__conda_setup"
-    else
-        if [ -f "/home/banerjee/.local/share/conda/etc/profile.d/conda.sh" ]; then
-            . "/home/banerjee/.local/share/conda/etc/profile.d/conda.sh"
-        else
-            export PATH="/home/banerjee/.local/share/conda/bin:$PATH"
-        fi
-    fi
-    unset __conda_setup
-    # <<< conda initialize <<<
-fi
