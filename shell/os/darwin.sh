7aa135244a134bc583eaa4077969f7ddcde36713
Rajit Banerjee <rajit.banerjee@ucdconnect.ie>
2022-01-19 16:11:59 +0000

    Switch to java 17

--- shell/os/darwin.sh
+++ shell/os/darwin.sh
@@ -10,4 +10,5 @@ fi
 ## Java
 export JAVA_8_HOME=$(/usr/libexec/java_home -v1.8)
 export JAVA_11_HOME=$(/usr/libexec/java_home -v11)
-export JAVA_HOME=$JAVA_8_HOME
+export JAVA_17_HOME=$(/usr/libexec/java_home -v17)
+export JAVA_HOME=$JAVA_17_HOME
