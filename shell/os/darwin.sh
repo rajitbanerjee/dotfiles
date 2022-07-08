d9781fac66c488538faf0e0c394ec88fe6463b34
Rajit Banerjee <rajit.banerjee@ucdconnect.ie>
2022-07-08 14:02:24 +0100

    feat: Make java 11 default

--- shell/os/darwin.sh
+++ shell/os/darwin.sh
@@ -1,6 +1,7 @@
 ## Python (macOS default)
 export PATH=~/Library/Python/3.8/bin:$PATH
 # export PATH=~/Library/Python/3.9/bin:$PATH
+# alias python="python3"
 
 ## command-not-found
 HB_CNF_HANDLER="$(brew --repository)/Library/Taps/homebrew/homebrew-command-not-found/handler.sh"
@@ -12,4 +13,5 @@ fi
 export JAVA_8_HOME=$(/usr/libexec/java_home -v1.8)
 export JAVA_11_HOME=$(/usr/libexec/java_home -v11)
 export JAVA_17_HOME=$(/usr/libexec/java_home -v17)
-export JAVA_HOME=$JAVA_17_HOME
+export JAVA_18_HOME=$(/usr/libexec/java_home -v18)
+export JAVA_HOME=$JAVA_11_HOME
