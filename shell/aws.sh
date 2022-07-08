d64954cf82b284b701e42582dc568e8008c7df53
Rajit Banerjee <rajit.banerjee@ucdconnect.ie>
2022-07-08 14:51:09 +0100

    feat: NodeIsengard aws config

--- shell/aws.sh
+++ shell/aws.sh
@@ -3,3 +3,5 @@ export PATH=~/.toolbox/bin:$PATH
 
 # AWS CLI completion
 complete -C '/usr/local/bin/aws_completer' aws
+
+export AWS_CONFIG_FILE="$HOME/Git/projects/NodeIsengard/profiles/isengardcli"
