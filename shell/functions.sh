587ecd28053f23f683e263ed9571f64c04319ffe
Rajit Banerjee <rajit.banerjee@ucdconnect.ie>
2022-07-12 15:25:52 +0100

    feat: Add custom netstat tcp ports display

--- shell/functions.sh
+++ shell/functions.sh
@@ -1,22 +1,27 @@
 # pretty print a csv file
 csv() {
-    column -t -s, "$1" | less -S
+	column -t -s, "$1" | less -S
 }
 
 # force set hostname (to prevent network from setting it)
 sethost() {
-    sudo scutil --set HostName "$1"
+	sudo scutil --set HostName "$1"
 }
 
 # convert markdown to pdf
 mkpdf() {
-    filename="$1"
-    base="${filename%.*}"
-    pandoc -o $base.pdf --pdf-engine=/Library/TeX/texbin/pdflatex $base.md
-    echo "File created: $base.pdf"
+	filename="$1"
+	base="${filename%.*}"
+	pandoc -o $base.pdf --pdf-engine=/Library/TeX/texbin/pdflatex $base.md
+	echo "File created: $base.pdf"
 }
 
 # better diff
 diff() {
-    command diff -u "$1" "$2" | delta
+	command diff -u "$1" "$2" | delta
+}
+
+# TCP ports in use
+ports() {
+	netstat -Watnlv | grep LISTEN | awk '{"ps -o comm= -p " $9 | getline procname;colred="\033[01;31m";colclr="\033[0m"; print colred "proto: " colclr $1 colred " | addr.port: " colclr $4 colred " | pid: " colclr $9 colred " | name: " colclr procname;  }' | column -t -s "|"
 }
