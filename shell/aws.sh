b37cb2b88b7680df4cd3cf48fccbbf290b534b90
Rajit Banerjee <rajit.banerjee@ucdconnect.ie>
2022-08-22 07:31:38 +0000

    feat: Update NodeIsengard path

--- shell/aws.sh
+++ shell/aws.sh
@@ -4,4 +4,4 @@ export PATH=~/.toolbox/bin:$PATH
 # AWS CLI completion
 complete -C "$(brew --prefix)/bin/aws_completer" aws
 
-export AWS_CONFIG_FILE=~/workplace/Misc/src/NodeIsengard/profiles/isengardcli
+export AWS_CONFIG_FILE=~/workplace/NodeIsengard/src/NodeIsengard/profiles/isengardcli
