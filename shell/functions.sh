01a1529ff94433016b1382d9ff78c8d019d5a450
Rajit Banerjee <rajit.banerjee@ucdconnect.ie>
2022-11-04 16:23:34 +0000

    feat: Add function to simplify git rebase

--- shell/functions.sh
+++ shell/functions.sh
@@ -25,3 +25,8 @@ diff() {
 ports() {
 	netstat -Watnlv | grep LISTEN | awk '{"ps -o comm= -p " $9 | getline procname;colred="\033[01;31m";colclr="\033[0m"; print colred "proto: " colclr $1 colred " | addr.port: " colclr $4 colred " | pid: " colclr $9 colred " | name: " colclr procname;  }' | column -t -s "|"
 }
+
+# git rebase
+grb() {
+    git rebase -i "HEAD~$1"
+}
