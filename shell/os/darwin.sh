32aa7eb78d7152cdf633a88e5678bf0f3c4fc762
Rajit Banerjee <rajit.banerjee@ucdconnect.ie>
2022-11-12 22:03:06 +0000

    chore: Remove command-not-found

--- shell/os/darwin.sh
+++ shell/os/darwin.sh
@@ -6,12 +6,6 @@ export PATH=~/Library/Python/3.8/bin:$PATH
 export PATH=~/Library/Python/3.9/bin:$PATH
 eval "$(pyenv init --path)"
 
-# command-not-found
-HB_CNF_HANDLER="$(brew --repository)/Library/Taps/homebrew/homebrew-command-not-found/handler.sh"
-if [ -f "$HB_CNF_HANDLER" ]; then
-	. "$HB_CNF_HANDLER"
-fi
-
 # Java
 export JAVA_8_HOME=$(/usr/libexec/java_home -v1.8)
 export JAVA_11_HOME=$(/usr/libexec/java_home -v11)
