8949fc2a547fe6b8d728d9efc099512bc49ee215
Rajit Banerjee <rajitbanerjee15@gmail.com>
2023-01-23 12:14:08 +0000

    feat: Switch Java to Corretto 11

--- shell/shared/common.sh
+++ shell/shared/common.sh
@@ -36,9 +36,9 @@ elif [ "$(uname)" = "Linux" ]; then
 	eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 	# Java
-	export PATH="$(brew --prefix)/Cellar/openjdk@11/11.0.16.1_1/bin:$PATH"
-	export JAVA_11_HOME="$(brew --prefix)/Cellar/openjdk@11/11.0.16.1_1/libexec"
-	export JAVA_HOME=$JAVA_11_HOME
+	export PATH="/usr/lib/jvm/java-11-amazon-corretto.x86_64/bin:$PATH"
+	export JAVA_11_AMAZON_CORRETO_HOME="/usr/lib/jvm/java-11-amazon-corretto.x86_64/"
+	export JAVA_HOME=$JAVA_11_AMAZON_CORRETO_HOME
 
 fi
 
