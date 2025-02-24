3940846dafc6fa1d0f0d35389d6b04aa60d73338
Rajit Banerjee <rajitbanerjee15@gmail.com>
2025-02-24 14:55:15 +0000

    fix: Avoid duplicate lualine

--- config/nvim/lua/plugins/appearance.lua
+++ config/nvim/lua/plugins/appearance.lua
@@ -56,6 +56,21 @@ return {
                 },
                 sections = { lualine_c = {} },
             })
+
+            -- Avoid duplicate lualine on changing colorscheme
+            local lualine_nvim_opts = require "lualine.utils.nvim_opts"
+            local base_set = lualine_nvim_opts.set
+
+            lualine_nvim_opts.set = function(name, val, scope)
+                if vim.env.TMUX ~= nil and name == "statusline" then
+                    if scope and scope.window == vim.api.nvim_get_current_win() then
+                        vim.g.tpipeline_statusline = val
+                        vim.cmd "silent! call tpipeline#update()"
+                    end
+                    return
+                end
+                return base_set(name, val, scope)
+            end
         end,
     },
     {
