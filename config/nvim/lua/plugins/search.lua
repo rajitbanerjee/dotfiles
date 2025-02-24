f5e586bb7ceb8cad7b24b6dd686f5eb1f3f7cc6c
Rajit Banerjee <rajitbanerjee15@gmail.com>
2025-02-24 21:22:28 +0000

    feat: Upgrade telescope config

--- config/nvim/lua/plugins/search.lua
+++ config/nvim/lua/plugins/search.lua
@@ -8,24 +8,33 @@ return {
         },
         config = function()
             require("telescope").setup({})
+        end,
+        keys = function()
             local telescope = require("telescope.builtin")
             vim.g.rooter_patterns = { "*.code-workspace", "packageInfo" }
-            vim.keymap.set("n", "<leader>t", ":Telescope<CR>")
-            vim.keymap.set("n", "<leader>f", telescope.find_files)
-            vim.keymap.set("n", "<leader>g", telescope.live_grep)
-            vim.keymap.set("n", "*", function()
-                telescope.live_grep({
-                    default_text = vim.fn.expand('<cword>')
-                })
-            end)
-            vim.keymap.set("n", "<leader>/", function()
-                telescope.current_buffer_fuzzy_find(
-                    require('telescope.themes').get_dropdown({
-                        winblend = 10,
-                        previewer = false,
-                    })
-                )
-            end)
+
+            return {
+                { "t",         ":Telescope<CR>",     desc = "Telescope: List" },
+                { "f",         telescope.oldfiles,   desc = "Telescope: MRU" },
+                { "<leader>f", telescope.find_files, desc = "Telescope: Find" },
+                { "<leader>g", telescope.live_grep,  desc = "Telescope: Grep" },
+                {
+                    "*",
+                    function()
+                        telescope.live_grep({ default_text = vim.fn.expand("<cword>") })
+                    end,
+                    desc = "Telescope: Grep Word"
+                },
+                {
+                    "<leader>/",
+                    function()
+                        telescope.current_buffer_fuzzy_find(
+                            require('telescope.themes').get_dropdown({ winblend = 10, previewer = false })
+                        )
+                    end,
+                    desc = "Telescope: Find In Buffer"
+                },
+            }
         end
     },
 }
