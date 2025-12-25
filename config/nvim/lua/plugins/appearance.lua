cd53f3873138f175430565d0980c0a22667e8456
Rajit Banerjee <rajitbanerjee15@gmail.com>
2025-12-25 17:18:53 +0000

    chore: delete nvim comfortable motion and tweak gruvbox

--- config/nvim/lua/plugins/appearance.lua
+++ config/nvim/lua/plugins/appearance.lua
@@ -10,10 +10,7 @@ return {
                 palette_overrides = {
                     dark0 = "#000000",
                     dark1 = "#111111",
-                    dark2 = "#222222",
-                    dark3 = "#333333",
-                    dark4 = "#444444",
-                }
+                },
             })
             vim.cmd.colorscheme "gruvbox"
         end,
